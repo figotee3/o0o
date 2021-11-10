f7739b6590e12791735edfc2a1ab14a4167eb579
figotee3 <figotee@hotmail.com>
2021-11-10 17:54:47 +0800

    ffiasdk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import re
 import long_responses as long
-
-
+aaaa
 def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
     message_certainty = 0
     has_required_words = True
